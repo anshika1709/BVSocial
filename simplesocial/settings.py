8067a62b51388d936357d8d4f685532319df61b8
Anshika Awasthi <46381790+anshika1709@users.noreply.github.com>
2020-11-04 16:13:31 +0530

    Update settings.py

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'bootstrap3',
     'accounts',
     'groups',
     'posts',
